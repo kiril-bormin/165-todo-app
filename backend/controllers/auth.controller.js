aa72d837c3af5ef22225a5252e55e6c07c2c672f
Kiril Bormin <borminnkiril@gmail.com>
2026-04-20 09:17:07 +0200

    fix connection sur le compte

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -6,41 +6,38 @@ const { JWT_SECRET } = require('../config/keys');
 // remove password from user object
 const cleanUser = (user) => {
   // eslint-disable-next-line no-unused-vars
-  const { password, ...cleanedUser } = user.get({ plain: true });
+  const { password, ...cleanedUser } = user.toObject();
   return cleanedUser;
 };
 
 const AuthController = {
   loginUser: async (req, res) => {
     const { User } = req.app.locals.models;
-    await User.findOne({
-      where: { email: req.body.email.toLowerCase() }
-    })
-      .then((result) => {
-        if (result) {
-          if (bcrypt.compareSync(req.body.password, result.password)) {
-            const user = cleanUser(result);
-            const token = jsonwebtoken.sign({}, JWT_SECRET, {
-              subject: result.id.toString(),
-              expiresIn: 60 * 60 * 24 * 30 * 6,
-              algorithm: 'RS256'
-            });
-            return res.status(200).json({ user: user, token: token });
-          } else {
-            return res.status(400).json({
-              message: 'Mauvais email ou mot de passe!'
-            });
-          }
+    try {
+      const result = await User.findOne({ email: req.body.email.toLowerCase() });
+      if (result) {
+        if (bcrypt.compareSync(req.body.password, result.password)) {
+          const user = cleanUser(result);
+          const token = jsonwebtoken.sign({}, JWT_SECRET, {
+            subject: result._id.toString(),
+            expiresIn: 60 * 60 * 24 * 30 * 6,
+            algorithm: 'RS256'
+          });
+          return res.status(200).json({ user: user, token: token });
         } else {
-          return res.status(404).json({
-            message: "Ce compte n'existe pas !"
+          return res.status(400).json({
+            message: 'Mauvais email ou mot de passe!'
           });
         }
-      })
-      .catch((error) => {
-        console.error('LOGIN USER: ', error);
-        return res.status(400).json(null);
-      });
+      } else {
+        return res.status(404).json({
+          message: "Ce compte n'existe pas !"
+        });
+      }
+    } catch (error) {
+      console.error('LOGIN USER: ', error);
+      return res.status(400).json(null);
+    }
   }
 };
 
